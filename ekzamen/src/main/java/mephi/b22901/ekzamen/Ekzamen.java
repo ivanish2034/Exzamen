51067bd30181c4eba008477dd5799308334b68c4
ivanish2034 <ivanish.0119@gmail.com>
2025-06-12 17:09:54 +0300

    Создание проекта
    Создал свой проект)

---
+++ ekzamen/src/main/java/mephi/b22901/ekzamen/Ekzamen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mephi.b22901.ekzamen;
+
+/**
+ *
+ * @author ivis2
+ */
+public class Ekzamen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Ekzamen!");
+    }
+}
